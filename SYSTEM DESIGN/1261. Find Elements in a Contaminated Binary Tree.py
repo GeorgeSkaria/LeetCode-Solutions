1cf33ba47524de6836a22f76ce6e87524eea0ba9
George Skaria Kattakayam <114094496+GeorgeSkaria@users.noreply.github.com>
2025-02-21 12:05:47 +0530

    Create 1261. Find Elements in a Contaminated Binary Tree.py
    tree problem

---
+++ SYSTEM DESIGN/1261. Find Elements in a Contaminated Binary Tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.values = {0}
+        root.val = 0
+        def create(node):
+            if not node:
+                return
+            self.values.add(node.val)
+            if node.left:
+                node.left.val = node.val * 2 + 1
+                create(node.left)
+            if node.right:
+                node.right.val = node.val * 2 + 2
+                create(node.right)
+        create(root)
+        
+
+    def find(self, target: int) -> bool:
+        return target in self.values
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
